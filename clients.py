90b581af224ce423a70ecd7d90671ea2590eac8a
Chengxin Cai <chengxin.cai@gmail.com>
2021-03-10 09:36:07 +0800

    update

--- clients.py
+++ clients.py
@@ -7,8 +7,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-USER = "mtest"
-PASS = "Cadence"
+USER, PASS = "mtest", "Cadence"
 HEADERS = {'accept': 'application/json'}
 
 
@@ -16,7 +15,7 @@ def log_trace(msg):
     print(msg, file=sys.stderr)
 
 
-def get_token(token_url, auth=(USER, PASS)):
+def get_token(auth, token_url: str = None):
     log_trace(f'Get {token_url}')
     resp = requests.get(token_url, auth=auth, verify=False)
     rr = resp.json()
@@ -30,7 +29,7 @@ class RestServer(object):
         self.using_sso = using_sso
         self.token_url = token_url
         if self.using_sso:
-            self.token = get_token(self.token_url, self.auth)
+            self.token = get_token(self.auth, self.token_url)
 
     def _get(self, path):
         url = f'{self.api_url}/{path}'
@@ -105,22 +104,48 @@ class ClouderaManager(RestServer):
         return f'https://{self.knox_host}:8443/gateway/cdp-proxy-api/knoxtoken/api/v1/token'
 
     def get_atlas_url(self):
-        return f'https://{self.atlas_host}:31000/api/atlas'
+        return f'http://{self.atlas_host}:31000/api/atlas'
 
     def get_atlas_knox_url(self):
         return f'https://{self.knox_host}:8443/gateway/cdp-proxy/atlas/api/atlas'
 
+    def get_ranger_url(self):
+        return f'http://{self.ranger_host}:6080'
+
     def get_ranger_knox_url(self):
         return f'https://{self.knox_host}:8443/gateway/cdp-proxy/ranger'
 
+    def get_livy_knox_url(self):
+        return f'https://{self.knox_host}:8443/gateway/cdp-proxy-api/livy'
+
+    def get_livy_jwt_url(self):
+        return f'https://{self.knox_host}:8443/gateway/jwt/livy'
+
 
 class Ranger(RestServer):
+    VER = "v2"
     def __init__(self, url, auth=(USER, PASS), using_sso=True, token_url=None):
         super().__init__(url, auth=auth,
                          using_sso=using_sso, token_url=token_url)
 
     def get_user_by_name(self, user):
         resp = self._get(f'service/xusers/users/userName/{user}')
+        rr = resp.json()
+        return rr
+
+    def get_groups_by_uid(self, uid):
+        resp = self._get(f'service/xusers/{uid}/groups')
+        rr = resp.json()
+        return rr
+
+    def get_policies(self, service_name=None, policy_name=None):
+        if service_name is None:
+            resp = self._get(f'service/public/{self.VER}/api/policy')
+        else:
+            if policy_name is None:
+                resp = self._get(f'service/public/{self.VER}/api/service/{service_name}/policy')
+            else:
+                resp = self._get(f'service/public/{self.VER}/api/service/{service_name}/policy/{policy_name}')
         rr = resp.json()
         return rr
 
@@ -134,12 +159,21 @@ class Atlas(RestServer):
     def __repr__(self):
         return f'<{self.__class__.__name__}: {self.api_url}>'
 
+    def setup_types(self, cfg_fname):
+        atlas_types = json.load(open(cfg_fname))
+        resp = self._post('types/typedefs', atlas_types)
+        return resp.status_code, resp.json()
+
+    def get_types(self):
+        resp = self._get('types/typedefs')
+        return resp.status_code, resp.json()
+
     def get_type_by_name(self, type_name):
         resp = self._get(f'types/typedef/name/{type_name}')
         return resp.status_code, resp.json()
 
-    def get_types(self):
-        resp = self._get(f'types/typedefs')
+    def get_classification_by_name(self, class_name):
+        resp = self._get(f'types/classificationdef/name/{class_name}')
         return resp.status_code, resp.json()
 
     def get_type_attrs(self, type_name):
@@ -150,10 +184,13 @@ class Atlas(RestServer):
         resp = self._delete(f'entity/guid/{guid}')
         return resp.status_code, resp.json()
 
-    def add_entity(self, entity_type, **kwargs):
+    def add_entity(self, entity_type, classifications=None, **kwargs):
         data = {'entity': {'typeName': entity_type,
                            'attributes': kwargs}
                }
+        if classifications:
+            data['entity']['classifications'] = classifications
+
         resp = self._post('entity', data)
         return resp.status_code, resp.json()
 
@@ -161,7 +198,15 @@ class Atlas(RestServer):
         resp = self._get(f'entity/guid/{guid}')
         return resp.status_code, resp.json()
 
-    def get_entities(self, entity_type, *, attrs=None, **kwargs):
+    def get_entity_by_qualifiedName(self, entity_type, q_name):
+        resp = self._get(f'entity/uniqueAttribute/type/{entity_type}?attr:qualifiedName={q_name}')
+        return resp.status_code, resp.json()
+
+    def find_entity(self, entity_type, q_name):
+        _, data = self.get_entity_by_qualifiedName(entity_type, q_name)
+        return data.get('entity')
+
+    def get_entities(self, entity_type, classifications=None, *, attrs=None, **kwargs):
         conditions = [{'attributeName': key,
                        'operator': '=',
                        'attributeValue': val} for key, val in kwargs.items()]
@@ -169,7 +214,8 @@ class Atlas(RestServer):
             attrs = self.get_type_attrs(entity_type)
         data = {'typeName': entity_type,
                 'excludeDeletedEntities': 'true',
-                'attributes': attrs
+                'attributes': attrs,
+                'classifications': classifications,
                }
         if conditions:
             if len(conditions) == 1:
@@ -177,10 +223,110 @@ class Atlas(RestServer):
             else:
                 data['entityFilters'] = {'condition': 'AND',
                                          'criterion': conditions}
-
+        print(json.dumps(data, indent=4))
         resp = self._post(f'search/basic', data)
         return resp.status_code, resp.json()
 
+    def add_path(self, path):
+        return self.add_entity('hdfs_path',
+                               classifications=[{'typeName': 'dsg'}],
+                               path=path,
+                               qualifiedName=f'{path}@cm',
+                               name=f'{path}@cm',
+                               isFile='false')
+
+    def add_app(self, prefix, identifier, name):
+        return self.add_entity('CDNSApp',
+                               qualifiedName=f'{prefix}:{identifier}',
+                               name=f'{prefix}:{identifier}',
+                               identifier=identifier,
+                               prefix=prefix,
+                               appName=name,
+                               owner='mtest',
+                               accessGroups='mozart_ml_dsg, cadence1, cadence_2'
+                               )
+
+    def add_design(self, domain, design_name):
+        return self.add_entity('CDNSDesign',
+                               qualifiedName=f'{domain}:{design_name}',
+                               name=f'{domain}:{design_name}',
+                               owner='mozart',
+                               description='design_name')
+
+    def add_run(self, domain, design_name, run_name):
+        attrs = {'qualifiedName': f'{domain}:{design_name}:{run_name}',
+                 'name': f'{domain}:{design_name}:{run_name}',
+                 'owner': 'mozart',
+                 'description': 'run'}
+
+        design = self.find_entity('CDNSDesign', f'{domain}:{design_name}')
+        if design:
+            attrs['design'] = {'guid': design['guid']}
+        return self.add_entity('CDNSRun', **attrs)
+
+    def add_stage(self, domain, design_name, run_name, stage_name):
+        attrs = {'qualifiedName': f'{domain}:{design_name}:{run_name}:{stage_name}',
+                 'name': f'{domain}:{design_name}:{run_name}:{stage_name}',
+                 'owner': 'mozart',
+                 'description': 'stage'}
+
+        run = self.find_entity('CDNSRun', f'{domain}:{design_name}:{run_name}')
+        if run:
+            attrs['run'] = {'guid': run['guid']}
+
+        design = self.find_entity('CDNSDesign', f'{domain}:{design_name}')
+        if design:
+            attrs['design'] = {'guid': design['guid']}
+
+        return self.add_entity('CDNSStage', **attrs)
+
+    def add_dataset(self, domain, db_id, design_name, run_name, stage_name,
+                    path, app_names):
+        attrs = {'qualifiedName': ':'.join([design_name, run_name,
+                                            stage_name, db_id]),
+                 'name': ':'.join([design_name, run_name, stage_name, db_id]),
+                 'displayText': ':'.join([design_name, run_name, stage_name, db_id]),
+                 'db_id': db_id,
+                 'fileSystem': 'hdfs://shmlbdlnx5.cadence.com:8020',
+                 'owner': 'mozart',
+                 'description': 'innovus',
+                 'accessGroups': 'mozart_ml_dsg, cadence1, cadence_2',
+                 'format': 'parquet',
+                 'ingestStatus': 'active',
+                 'stageName': stage_name,
+                 'runId': run_name,
+                 'designName': design_name,
+                 }
+
+        directory = self.find_entity('hdfs_path', f'{path}@cm')
+        attrs['directory'] = [{'guid': directory['guid']}]
+
+        design = self.find_entity('CDNSDesign', f'{domain}:{design_name}')
+        if design:
+            attrs['design'] = {'guid': design['guid']}
+
+        run = self.find_entity('CDNSRun', f'{domain}:{design_name}:{run_name}')
+        if run:
+            attrs['run'] = {'guid': run['guid']}
+
+        stage = self.find_entity('CDNSStage',
+                                 f'{domain}:{design_name}:{run_name}:{stage_name}')
+        if stage:
+            attrs['stage'] = {'guid': stage['guid']}
+
+        _, data = self.get_entities('CDNSApp')
+        all_apps = data.get('entities', [])
+        apps = []
+        for app_name in app_names.split(','):
+            apps.extend([{'guid': app['guid']} for app in all_apps
+                                               if app['attributes']['appName'] == app_name])
+        attrs['apps'] = apps
+        attrs['classifications'] = [
+                {'typeName': 'access',
+                 'attributes': {'user': ['#mtest#'],
+                                'groups': ['#mlsh#', '#mozart_ml_dsg#','#cadence_2#']}}]
+
+        return self.add_entity('CDNSDataSet', **attrs)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -196,4 +342,6 @@ if __name__ == '__main__':
     print(f'    AtlasUrl: {cm.get_atlas_url()}')
     print(f'    AtlasKnoxUrl: {cm.get_atlas_knox_url()}')
     print(f'    RangerKnoxUrl: {cm.get_ranger_knox_url()}')
+    print(f'    LivyKnoxUrl: {cm.get_livy_knox_url()}')
+    print(f'    LivyJwtUrl: {cm.get_livy_jwt_url()}')
 
